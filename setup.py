7913f4db904767c3ed60a1a6d747a639cbbfc0c3
Shohei1029 <t14650sn@sfc.keio.ac.jp>
2017-09-09 18:43:16 +0900

    update

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@
 from setuptools import setup, find_packages
  
 setup(
-        name             = 'SN_bioinfo_bits',
-        version          = '1.0.0',
-        description      = 'Collection of modules for dealing with biological data in Python, created by S.N.',
+        name             = "SN_bioinfo_bits",
+        version          = "1.0.0",
+        description      = "Collection of modules for dealing with biological data in Python, created by S.N.",
 #        license          = __license__,
-        author           = 'S.N.',
-        author_email     = '',
-        url              = 'https://github.com/shohei1029/SN_bioinfo_bits.git',
-        keywords         = 'bioinformatics SN',
+        author           = "S.N.",
+        author_email     = "",
+        url              = "https://github.com/shohei1029/SN_bioinfo_bits.git",
+        keywords         = ["bioinformatics", "SN"]
         packages         = find_packages(),
-        install_requires = [],
+        install_requires = ["numpy", "biopython", "matplotlib", "toml"],
+        extras_require   = ["progressbar2", "PypeR", "neovim", "ipdb", "ete3"]
         )
