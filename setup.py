2c63d2b0f5fdf44bebe7ef85faa2c4c0a21a7a87
Shohei1029 <t14650sn@sfc.keio.ac.jp>
2018-03-06 22:47:32 +0900

    add fasta2mat_prot.sh, fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
  
 setup(
     name             = 'biwand',
-    version          = '1.0.2.dev0',
+    version          = '1.0.2.dev1',
     description      = 'A wand for bioinformatician. Collection of modules for dealing with biological data in Python, created by S.N.',
      #license          = __license__,
     author           = 'S.N.',
@@ -14,6 +14,7 @@ setup(
     packages         = find_packages(),
     scripts          = [
         'bin/fasta2mat_nucl.sh',
+        'bin/fasta2mat_prot.sh',
         'bin/blast2sim.py',
         'bin/plot_hist.py',
         ],
