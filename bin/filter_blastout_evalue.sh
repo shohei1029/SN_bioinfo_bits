58dd3ea925a42f66fc7e09d2cb3d0c43bc2713fb
Shohei1029 <shohei@sfc.keio.ac.jp>
2018-10-12 18:54:30 +0900

    update

--- bin/filter_blastout_evalue.sh
+++ bin/filter_blastout_evalue.sh
@@ -7,4 +7,4 @@ if [ "$1" = "" ]; then
     EVALUE=1e-5
 fi
 
-grep -v "#" </dev/stdin | perl -F"\\t" -lane 'print if $F[10] <= $var' -- -var=${EVALUE}
+grep -v "#" </dev/stdin | perl -F"\\t" -slane 'print if $F[10] <= $var' -- -var=${EVALUE}
