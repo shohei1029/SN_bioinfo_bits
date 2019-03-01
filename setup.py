d2f8d69b36f0a85002a2ff609c0fe524314b49a9
Shohei1029 <shohei@sfc.keio.ac.jp>
2019-03-01 17:19:28 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
  
 setup(
     name             = 'biwand',
-    version          = '1.0.7.dev1',
+    version          = '1.0.7.dev2',
     description      = 'A wand for bioinformatician. Collection of modules for dealing with biological data in Python, created by S.N.',
      #license          = __license__,
     author           = 'S.N.',
